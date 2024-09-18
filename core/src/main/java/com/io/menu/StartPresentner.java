0e3ab3398b8b95a5281cb83fe499ef7e35727851
kubickipi314 <kubicki.pi314@gmail.com>
2024-09-18 20:50:59 +0200

    selected figures update <3

---
+++ core/src/main/java/com/io/menu/StartPresentner.java
@@ -0,0 +1,11 @@
+package com.io.menu;
+
+public class StartPresentner {
+
+    public void update() {
+
+    }
+
+    public void render() {
+    }
+}
---
+++ core/src/main/java/com/io/screens/StartScreen.java
@@ -0,0 +1,53 @@
+package com.io.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.io.menu.StartPresentner;
+
+public class StartScreen implements Screen {
+
+    private final StartPresentner startPresenter;
+
+    public StartScreen(StartPresentner startPresenter) {
+        this.startPresenter = startPresenter;
+    }
+
+    @Override
+    public void show() {
+        Gdx.gl.glClearColor(0.12f, 0.12f, 0.12f, 1);
+    }
+
+    @Override
+    public void render(float delta) {
+        startPresenter.update();
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        startPresenter.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy screen's assets here.
+    }
+}
