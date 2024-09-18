b74ae55cb9b2ab22cf729c607f0a0da0f650467e
Adam Szwaja <szwajaadam@gmail.com>
2024-09-18 21:31:23 +0200

    improve bishop test

--- core/src/test/java/com/io/core/moves/BishopMoveTest.java
+++ core/src/test/java/com/io/core/moves/BishopMoveTest.java
@@ -1,18 +1,47 @@
 package com.io.core.moves;
 
+import com.io.core.board.Board;
+import com.io.core.board.BoardPosition;
+import com.io.core.board.Cell;
+import com.io.core.character.Player;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class BishopMoveTest {
 
     @Test
     void testIsValidMove_NoObstacle() {
-        BishopMove bishopMove = new BishopMove(5, 10);
+        BishopMove bishopMove = new BishopMove(10, 5);
+
+        var mockBoard = mock(Board.class);
+        when(mockBoard.getCell(any(BoardPosition.class))).thenReturn(new Cell(false));
+        when(mockBoard.isValidCell(any(BoardPosition.class))).thenReturn(true);
+        var startPostion = new BoardPosition(0, 0);
+        var mockPlayer = mock(Player.class);
+        when(mockPlayer.getPosition()).thenReturn(startPostion);
+
+
+        assertTrue(bishopMove.isMoveValid(mockPlayer, new BoardPosition(3, 3), mockBoard));
     }
 
     @Test
-    void getAccessibleCells() {
+    void getAccessibleCells_Obstacle() {
+        BishopMove bishopMove = new BishopMove(10, 5);
+
+        var mockBoard = mock(Board.class);
+        when(mockBoard.getCell(any(BoardPosition.class))).thenReturn(new Cell(false));
+        when(mockBoard.isValidCell(any(BoardPosition.class))).thenReturn(true);
+        when(mockBoard.isValidCell(new BoardPosition(2, 2))).thenReturn(false);
+        var startPosition = new BoardPosition(0, 0);
+        var mockPlayer = mock(Player.class);
+        when(mockPlayer.getPosition()).thenReturn(startPosition);
+
+
+        assertFalse(bishopMove.isMoveValid(mockPlayer, new BoardPosition(3, 3), mockBoard));
     }
 
     @Test
