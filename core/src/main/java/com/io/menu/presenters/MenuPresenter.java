4ce5c61f0acf2c09c28ac340e578eb24f3b7ca6a
kubickipi314 <kubicki.pi314@gmail.com>
2024-09-19 14:51:21 +0200

    selection sounds added

--- core/src/main/java/com/io/menu/presenters/MenuPresenter.java
+++ core/src/main/java/com/io/menu/presenters/MenuPresenter.java
@@ -6,19 +6,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.io.Coordinator;
 import com.io.menu.view.MenuButton;
+import com.io.menu.view.MenuSoundManager;
 import com.io.menu.view.MenuTextureManager;
 
 public class MenuPresenter {
     private final Coordinator coordinator;
+    private final MenuSoundManager sm;
     private final SpriteBatch batch;
     private final MenuButton backButton;
     private final MenuButton levelButton;
     private final float windowHeight;
-    public MenuPresenter(Coordinator coordinator){
+
+    public MenuPresenter(Coordinator coordinator) {
         this.coordinator = coordinator;
 
         batch = new SpriteBatch();
         MenuTextureManager tm = new MenuTextureManager();
+        sm = new MenuSoundManager();
+
         float windowWidth = Gdx.graphics.getWidth();
         windowHeight = Gdx.graphics.getHeight();
         float tileSize = windowHeight / 5;
@@ -26,7 +31,7 @@ public class MenuPresenter {
         backButton = new MenuButton(tm.getBackButton(), playPosition, tileSize, tileSize);
 
         Vector2 characterPosition = new Vector2(windowWidth / 2 - tileSize * 1.5f, tileSize * 1.5f);
-        levelButton = new MenuButton(tm.getLevel(), characterPosition, 3*tileSize, 3*tileSize);
+        levelButton = new MenuButton(tm.getLevel(), characterPosition, 3 * tileSize, 3 * tileSize);
     }
 
     public void update() {
@@ -34,18 +39,20 @@ public class MenuPresenter {
         levelButton.unmark();
 
         Vector2 mousePosition = getMousePosition();
-        if (backButton.contains(mousePosition)){
+        if (backButton.contains(mousePosition)) {
             backButton.mark();
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-                System.out.println("PLAY!!!");
+                System.out.println("Go back");
+                sm.playSelectSound();
                 coordinator.setStartScreen();
             }
         }
 
-        if (levelButton.contains(mousePosition)){
+        if (levelButton.contains(mousePosition)) {
             levelButton.mark();
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
                 System.out.println("Start game level");
+                sm.playSelectSound();
                 coordinator.setGameScreen();
             }
         }
--- core/src/main/java/com/io/menu/presenters/StartPresenter.java
+++ core/src/main/java/com/io/menu/presenters/StartPresenter.java
@@ -6,33 +6,38 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.io.Coordinator;
 import com.io.menu.view.MenuButton;
+import com.io.menu.view.MenuSoundManager;
 import com.io.menu.view.MenuTextureManager;
 
 public class StartPresenter {
     private final Coordinator coordinator;
+    private final MenuSoundManager sm;
     private final SpriteBatch batch;
     private final MenuButton playButton;
     private final MenuButton quitButton;
     private final MenuButton playerButton;
     private final float windowHeight;
-    public StartPresenter(Coordinator coordinator){
+
+    public StartPresenter(Coordinator coordinator) {
         this.coordinator = coordinator;
 
         batch = new SpriteBatch();
         MenuTextureManager tm = new MenuTextureManager();
+        sm = new MenuSoundManager();
+
         float windowWidth = Gdx.graphics.getWidth();
         windowHeight = Gdx.graphics.getHeight();
         float tileSize = windowHeight / 7;
         float mainY = tileSize / 2;
 
         Vector2 quitPosition = new Vector2(windowWidth / 2 - tileSize, mainY);
-        quitButton = new MenuButton(tm.getQuitButton(), quitPosition, tileSize, 2*tileSize);
+        quitButton = new MenuButton(tm.getQuitButton(), quitPosition, tileSize, 2 * tileSize);
 
-        Vector2 playPosition = new Vector2(windowWidth / 2 - 1.5f * tileSize, mainY+tileSize);
-        playButton = new MenuButton(tm.getPlayButton(), playPosition, tileSize, 3*tileSize);
+        Vector2 playPosition = new Vector2(windowWidth / 2 - 1.5f * tileSize, mainY + tileSize);
+        playButton = new MenuButton(tm.getPlayButton(), playPosition, tileSize, 3 * tileSize);
 
         Vector2 characterPosition = new Vector2(windowWidth / 2 - tileSize, mainY + tileSize * 2.2f);
-        playerButton = new MenuButton(tm.getPlayer(), characterPosition, 2*tileSize, 2*tileSize);
+        playerButton = new MenuButton(tm.getPlayer(), characterPosition, 2 * tileSize, 2 * tileSize);
     }
 
     public void update() {
@@ -42,22 +47,25 @@ public class StartPresenter {
 
         Vector2 mousePosition = getMousePosition();
 
-        if (quitButton.contains(mousePosition)){
+        if (quitButton.contains(mousePosition)) {
             quitButton.mark();
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
                 System.out.println("QUIT!!!");
+                sm.playSelectSound();
                 coordinator.quit();
             }
-        } else if (playButton.contains(mousePosition)){
+        } else if (playButton.contains(mousePosition)) {
             playButton.mark();
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
                 System.out.println("PLAY!!!");
+                sm.playSelectSound();
                 coordinator.setMenuScreen();
             }
-        } else if (playerButton.contains(mousePosition)){
+        } else if (playerButton.contains(mousePosition)) {
             playerButton.mark();
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
                 System.out.println("Choose player");
+                sm.playSelectSound();
             }
         }
     }
