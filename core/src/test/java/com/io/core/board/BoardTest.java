b11311231629d70511d9b9f77b74241fa97334dd
Adam Szwaja <szwajaadam@gmail.com>
2024-09-20 18:05:54 +0200

    add partial board tests

--- core/src/test/java/com/io/core/board/BoardTest.java
+++ core/src/test/java/com/io/core/board/BoardTest.java
@@ -1,42 +1,58 @@
-//package com.io.core.board;
-//
-//import org.junit.jupiter.api.Test;
-//
-//class BoardTest {
-//
-//    @Test
-//    void testTryMakeMove() {
-//    }
-//
-//    @Test
-//    void testGetBoardWidth() {
-//    }
-//
-//    @Test
-//    void testGetBoardHeight() {
-//    }
-//
-//    @Test
-//    void testIsValidCell() {
-//    }
-//
-//    @Test
-//    void testGetCharacter() {
-//    }
-//
-//    @Test
-//    void testGetCharacters() {
-//    }
-//
-//    @Test
-//    void testGetTeamPosition() {
-//    }
-//
-//    @Test
-//    void testDecreaseTeamCount() {
-//    }
-//
-//    @Test
-//    void testGetTeamCount() {
-//    }
-//}
+package com.io.core.board;
+
+import com.io.core.character.Character;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BoardTest {
+
+    @Test
+    void testTryMakeMove() {
+    }
+
+    @Test
+    void testGetBoardWidth() {
+        Board board = new Board(10, 20, new ArrayList<>());
+        int result = board.getBoardWidth();
+        assertEquals(10, result);
+    }
+
+    @Test
+    void testGetBoardHeight() {
+        Board board = new Board(10, 20, new ArrayList<>());
+        int result = board.getBoardHeight();
+        assertEquals(20, result);
+    }
+
+    @Test
+    void testIsValidCell() {
+    }
+
+    @Test
+    void testGetCharacter() {
+    }
+
+    @Test
+    void testGetCharacters() {
+        List<Character> mockList = new ArrayList<>();
+        Board board = new Board(10, 20, mockList);
+        var result = board.getCharacters();
+        assertEquals(result, mockList);
+    }
+
+    @Test
+    void testGetTeamPosition() {
+    }
+
+    @Test
+    void testDecreaseTeamCount() {
+    }
+
+    @Test
+    void testGetTeamCount() {
+    }
+}
