d0b0379719db09d4a69c1760c8b026841c950fa7
Marwyk2003 <marwyk2003@gmail.com>
2024-09-23 21:20:24 +0200

    fix-merge

--- core/src/main/java/com/io/core/board/BoardMoveChange.java
+++ core/src/main/java/com/io/core/board/BoardMoveChange.java
@@ -1,21 +1,20 @@
 package com.io.core.board;
 
-import com.io.core.character.Character;
 import com.io.core.moves.MoveDTO;
-import com.io.service.CharacterInterface;
+import com.io.service.game.CharacterInterface;
 
 public record BoardMoveChange(
-    boolean success,
-    boolean hasMoved,
-    boolean hasAttacked,
-    MoveDTO move,
-    CharacterInterface attacked
-) {
-    static BoardMoveChange SUCCESS(
+        boolean success,
         boolean hasMoved,
         boolean hasAttacked,
         MoveDTO move,
         CharacterInterface attacked
+) {
+    static BoardMoveChange SUCCESS(
+            boolean hasMoved,
+            boolean hasAttacked,
+            MoveDTO move,
+            CharacterInterface attacked
     ) {
         return new BoardMoveChange(true, hasMoved, hasAttacked, move, attacked);
     }
